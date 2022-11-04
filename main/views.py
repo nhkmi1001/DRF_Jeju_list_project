50ff30bd7f79b0c61a05b982f2097965263ad56a
nhkmi1001 <nhkmi1001@gmail.com>
2022-11-04 11:21:59 +0900

    [Mod] Datebase modeling

--- main/views.py
+++ main/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 import pandas as pd
 import numpy as np
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from pprint import pprint
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
-import time
-import json
-import csv
+from tqdm import tqdm
+from django.conf import settings
+# settings.configure(DEBUG=True)
+import os , django, time
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DRF_Jeju_List.settings")
+# django.setup()
 from .models import Store
 
+def GetStoreId():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-def GetStoreID():
-# df = pd.read_csv('main/jejulist.csv', encoding='cp949')
-    df1 = pd.read_csv('jejulist.csv', encoding='UTF-8')
-    df = df1.head(3)
+        # df = pd.read_csv('main/jejulist.csv', encoding='cp949')
+    df1 = pd.read_csv('main/jejulist.csv', encoding='utf-8')
+    df = df1.head(1)
 
     jeju_store = df[['업소명','소재지','메뉴']]
     print(jeju_store.head())
@@ -36,7 +39,7 @@ def GetStoreID():
 
     jeju_store['kakao_keyword'] = jeju_store['address'] # "%20"는 띄어쓰기를 의미합니다.
     store_detail_list = []
-
+    store_id_list = []
 
     # 상세페이지 주서 따기
     for i, keyword in enumerate(jeju_store['kakao_keyword'].tolist()):
@@ -44,60 +47,54 @@ def GetStoreID():
         kakao_map_search_url = f"https://map.kakao.com/?q={keyword}"
 
         driver.get(kakao_map_search_url)
-        time.sleep(1)
+        time.sleep(2)
         df.iloc[i,-1] = driver.find_element(By.CSS_SELECTOR,"#info\.search\.place\.list > li > div.info_item > div.contact.clickArea > a.moreview").get_attribute('href')
 
         store_detail_list.append(df.iloc[i,-1])
-        # df.iloc[i,-1] = driver.find_element '//*[@id="info.search.place.list"]/li/div[5]/div[4]/a[1]').click()
-    print(store_detail_list)
 
-    for url in store_detail_list:
-        url = url.split('/')
-        print(url[-1])
-        Store(store_id=url[-1]).save()
+        for url in store_detail_list:
+            url = url.split('/')
+            print(url[-1])
+            Store(store_id=url[-1]).save()
+        # df.iloc[i,-1] = driver.find_element '//*[@id="info.search.place.list"]/li/div[5]/div[4]/a[1]').click()
+    pprint(store_detail_list)
+
 
     store_info = []
     for store in store_detail_list:
-        # driver.get(https://place.map.kakao.com/+f{'store_id'}) 주석풀고 이용하세요!
         driver.get(store)
-        time.sleep(1)
+        time.sleep(3)
         review_info = {
-            'store_name': '',
+            'store': '',
             'tags': [],
-            'contents': [],
+            'contents': []
         }
         review_info['star'] = driver.find_element(By.CLASS_NAME,"ahead_info").find_element(By.CLASS_NAME,"grade_star").find_element(By.CLASS_NAME, "num_rate").text
         reviews = driver.find_element(By.CLASS_NAME,"list_evaluation").find_elements(By.TAG_NAME, "li")
         for review in reviews:
             review_content = {}
-            review_tag ={}
+            review_tags = {}
             if not review.text : #or driver.find_element(By.CLASS_NAME,"group_likepoint").find_elements(By.TAG_NAME, "span"):
                 continue
             try:
                 tags = review.find_element(By.CLASS_NAME, "group_likepoint").find_elements(By.TAG_NAME, "span")
-                review_tag['tag'] = [x.text for x in tags]
+                review_tags= [x.text for x in tags]
             except NoSuchElementException:
-                review_tag['tag'] = None
+                review_tags = None
 
             try:
-                review_content['content'] = review.find_element(By.CLASS_NAME, "txt_comment").text
+                review_content = review.find_element(By.CLASS_NAME, "txt_comment").text
             except NoSuchElementException:
                 # 식별값(있지만 없어도 되는 값)
-                review_content['content'] = None
-
-            review_info['tags'].append(review_tag)
+                review_content = None
             review_info['contents'].append(review_content)
-            
+            review_info['tags'].append(review_tags)
+            print(review_content)
         store_info.append(review_info)
-        Store(tags=review_tag, contents=review_content).save()
-        
-    pprint(store_info)
-GetStoreID()
+        Store(menu=review_info['tags'], content=review_info['contents']).save()
 
+        # print(review_info)
+        # return review_info
 
-# def toJson(store_info):
-#     with open('store_info.json', 'w', encoding='utf-8') as file :
-#         json.dump(store_info, file, ensure_ascii=False, indent='\t')
-        
-# toJson(store_info)
+GetStoreId()
 
